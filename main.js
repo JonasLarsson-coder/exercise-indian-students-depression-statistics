c75c1d9541a36eb2090d5c9b0e8545d90f4da4df
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-03-21 11:31:44 +0100

    Added allStudents

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import addMdToPage from './libs/addMdToPage.js';
 import dbQuery from "./libs/dbQuery.js";
 import tableFromData from './libs/tableFromData.js';
 
-let allStudents = await dbQuery('SELECT * FROM results LIMIT 10');
+let allStudents = await dbQuery("SELECT * FROM results WHERE profession='Student' LIMIT 10");
 let allMale = await dbQuery("SELECT * FROM results WHERE gender='Male' LIMIT 10");
 let allFemale = await dbQuery("SELECT * FROM results WHERE gender='Female' LIMIT 10")
 
@@ -12,3 +12,5 @@ let allFemale = await dbQuery("SELECT * FROM results WHERE gender='Female' LIMIT
 tableFromData({ data: allStudents });
 tableFromData({ data: allMale });
 tableFromData({ data: allFemale });
+
+
