aeb2ba21949c83a0eac3b044d48f600837d01ee3
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-03-21 13:56:17 +0100

    update

--- main.js
+++ main.js
@@ -10,10 +10,12 @@ let suicidalThoughtsYes = await dbQuery("SELECT * FROM results WHERE suicidalTho
 let suicidalThoughtsNo = await dbQuery("SELECT * FROM results WHERE suicidalThoughts='0' LIMIT 10")
 let suicidalThoughtsProcent = await dbQuery("SELECT ROUND(AVG(suicidalThoughts) * 100, 2) || '%' AS percentage_suicidal FROM results");
 
-tableFromData({ data: allStudents });
+
+
+//tableFromData({ data: allStudents });
 tableFromData({ data: allMale });
-tableFromData({ data: allFemale });
-tableFromData({ data: suicidalThoughtsYes });
-tableFromData({ data: suicidalThoughtsNo });
-tableFromData({ data: suicidalThoughtsProcent });
+//tableFromData({ data: allFemale });
+//tableFromData({ data: suicidalThoughtsYes });
+//tableFromData({ data: suicidalThoughtsNo });
+//tableFromData({ data: suicidalThoughtsProcent });
 
