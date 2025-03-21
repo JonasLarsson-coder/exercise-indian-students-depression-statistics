cf5fba37c4a771dda2cf003b4e3407b325b8723d
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-03-21 12:32:07 +0100

    added suicidalThoughts i procent

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ import tableFromData from './libs/tableFromData.js';
 let allStudents = await dbQuery("SELECT * FROM results WHERE profession='Student' LIMIT 10");
 let allMale = await dbQuery("SELECT * FROM results WHERE gender='Male' LIMIT 10");
 let allFemale = await dbQuery("SELECT * FROM results WHERE gender='Female' LIMIT 10");
-let suicidalThoughtsYes = await dbQuery("SELECT * FROM results WHERE suicidalThoughts='Yes' LIMIT 10")
-let suicidalThoughtsNo = await dbQuery("SELECT * FROM results WHERE suicidalThoughts='No' LIMIT 10")
+let suicidalThoughtsYes = await dbQuery("SELECT * FROM results WHERE suicidalThoughts='1' LIMIT 10")
+let suicidalThoughtsNo = await dbQuery("SELECT * FROM results WHERE suicidalThoughts='0' LIMIT 10")
+let suicidalThoughtsProcent = await dbQuery("SELECT ROUND(AVG(suicidalThoughts) * 100, 2) || '%' AS percentage_suicidal FROM results");
 
 tableFromData({ data: allStudents });
 tableFromData({ data: allMale });
 tableFromData({ data: allFemale });
 tableFromData({ data: suicidalThoughtsYes });
 tableFromData({ data: suicidalThoughtsNo });
+tableFromData({ data: suicidalThoughtsProcent });
 
