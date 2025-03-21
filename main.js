ae935a3f76fe26a237a86e44e20497a210f10a57
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-03-21 11:36:49 +0100

    added suicidalThoughts yes and no

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ import tableFromData from './libs/tableFromData.js';
 
 let allStudents = await dbQuery("SELECT * FROM results WHERE profession='Student' LIMIT 10");
 let allMale = await dbQuery("SELECT * FROM results WHERE gender='Male' LIMIT 10");
-let allFemale = await dbQuery("SELECT * FROM results WHERE gender='Female' LIMIT 10")
-
-
+let allFemale = await dbQuery("SELECT * FROM results WHERE gender='Female' LIMIT 10");
+let suicidalThoughtsYes = await dbQuery("SELECT * FROM results WHERE suicidalThoughts='Yes' LIMIT 10")
+let suicidalThoughtsNo = await dbQuery("SELECT * FROM results WHERE suicidalThoughts='No' LIMIT 10")
 
 tableFromData({ data: allStudents });
 tableFromData({ data: allMale });
 tableFromData({ data: allFemale });
-
+tableFromData({ data: suicidalThoughtsYes });
+tableFromData({ data: suicidalThoughtsNo });
 
