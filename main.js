e8c904a54a521fe918dda0c46180957ac8670b75
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-03-21 10:59:50 +0100

    added filter for male and female

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import './libs/liveReload.js';
 import addMdToPage from './libs/addMdToPage.js';
 import dbQuery from "./libs/dbQuery.js";
-import tableFromData from './libs/tableFromData.js'
+import tableFromData from './libs/tableFromData.js';
 
 let allStudents = await dbQuery('SELECT * FROM results LIMIT 10');
+let allMale = await dbQuery("SELECT * FROM results WHERE gender='Male' LIMIT 10");
+let allFemale = await dbQuery("SELECT * FROM results WHERE gender='Female' LIMIT 10")
+
+
 
 tableFromData({ data: allStudents });
+tableFromData({ data: allMale });
+tableFromData({ data: allFemale });
